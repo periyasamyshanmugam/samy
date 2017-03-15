b5ab78fa12f36f59a9f3944162fac3c74a03ba5b
periyasamyshanmugam <periyasamy23shanmugam@gmail.com>
2017-03-16 00:57:30 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -1,38 +1,18 @@
 import java.util.*;
-
 import java.lang.*;
-
 import java.io.*;
-
 import java.util.Scanner;
-
-
 class oddeven
-
 {
-	
-	public static void main (String[] args) throws java.lang.Exception
-	
+	public static void main (String[] args)
 	{
-
-	
 	int n;
-
-		Scanner s = new Scanner(System.in);
-
-		System.out.print("Entere the number");
-
-		n = s.nextInt();
-		
-		if(n%2==0)
-			System.out.print("Entered number is Even number");
-
-		else
-
-			System.out.print("Entered number is Odd number");
-
-
+	Scanner s = new Scanner(System.in);
+	n = s.nextInt();
+	if(n%2==0)
+		System.out.print("Entered number is Even number");
+	else
+		System.out.print("Entered number is Odd number");
 	}
-
 }
 
