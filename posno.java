faea905d110a7ad10afffb89978f315c4d69b770
periyasamyshanmugam <periyasamy23shanmugam@gmail.com>
2017-03-16 00:30:33 +0530

    Add files via upload

---
+++ posno.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class posno
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner s = new Scanner(System.in);
+		n = s.nextInt();
+		if(n==0)
+		{
+			System.out.print("Entered number is Zero");
+		}
+		else if(n>0)
+		{
+			System.out.print("Entered number is Positive number");
+		}
+		else if(n<0)
+		{
+			System.out.print("Entered number is Negative number");
+		}
+		else
+		{
+			System.out.print("Invalid input");
+		}
+	}
+}
