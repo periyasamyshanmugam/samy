86203fa2acaa9dffbeabb96e02f0e6032f85f6ab
periyasamyshanmugam <periyasamy23shanmugam@gmail.com>
2017-03-16 01:56:36 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ 
+class Vowel
+{
+    public static void main(String[] args)  throws java.lang.Exception
+    {
+        int i;
+        for(i=0;i<5;i++)
+        {
+            System.out.print("Hello");
+        }
+    }
+    
+}
