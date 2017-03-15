00df2a94b6b5caf30442a798e6a38b18636ed90d
periyasamyshanmugam <periyasamy23shanmugam@gmail.com>
2017-03-16 00:31:36 +0530

    Update posno.java

--- posno.java
+++ posno.java
@@ -1,17 +1,14 @@
-/* package whatever; // don't place package name! */
+
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 import java.util.Scanner;
 
-
-/* Name of the class has to be "Main" only if the class is public. */
 class posno
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		int n;
 		Scanner s = new Scanner(System.in);
 		n = s.nextInt();
